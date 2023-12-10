933987164eb958259b51219241d7e68336288561
VINH <trivinh147@gmail.com>
2023-12-10 20:35:47 +0700

    Asset Mario Movement

--- Assets/Script/Mario_Script.cs
+++ Assets/Script/Mario_Script.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Mario_Script : MonoBehaviour
 {
+
+    public float Speed = 1f;
+    public bool Touch_Ground = true;
+    public bool Change = false;
+    private Animator animations;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animations = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        animations.SetFloat("Speed", Speed);
+        animations.SetBool("Touch_Ground", Touch_Ground);
+        animations.SetBool("Change", Change);
         
     }
 }
