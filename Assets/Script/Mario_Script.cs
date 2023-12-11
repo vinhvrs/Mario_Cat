ec383233c83dbc065c7624f35e131fbfbd589466
VINH <trivinh147@gmail.com>
2023-12-11 18:36:53 +0700

    Update Jump and Turn_Around

--- Assets/Script/Mario_Script.cs
+++ Assets/Script/Mario_Script.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class Mario_Script : MonoBehaviour
 {
-
-    public float Speed = 1f;
-    public bool Touch_Ground = true;
-    public bool Change = false;
+    public float velo = 5f;
+    private bool Turn_around = false;
+    private float Speed = 0;
+    private float Run_Jump = 0;
+    private float pre_y = -7.975002f;
+    private bool Touch_Ground = true;
+    private bool Change = false;
+    private bool Jump = false;
+    private Rigidbody2D body;
     private Animator animations;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        body = GetComponent<Rigidbody2D>();
         animations = GetComponent<Animator>();
     }
 
@@ -23,6 +31,56 @@ public class Mario_Script : MonoBehaviour
         animations.SetFloat("Speed", Speed);
         animations.SetBool("Touch_Ground", Touch_Ground);
         animations.SetBool("Change", Change);
-        
+        animations.SetBool("Jump", Jump);
+        pre_y = body.velocity.y;
+        Jump_Up();  
     }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        float Horizontal_Move = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2 (velo*Horizontal_Move, body.velocity.y);
+        Speed = Mathf.Abs(velo*Horizontal_Move);
+        Run_Jump = Speed;
+        if (Horizontal_Move > 0 && Turn_around) Change_Direct();
+        if (Horizontal_Move < 0 && !Turn_around) Change_Direct();
+        if (Speed == 0 )
+        {
+            Jump = false;
+            //Touch_Ground = true;
+        }
+    }
+
+    public void Change_Direct()
+    {
+        Turn_around = !Turn_around;
+        Vector2 Direction = transform.localScale;
+        Direction.x *= -1;
+        transform.localScale = Direction;
+    }
+
+    public void Jump_Up()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space) && Touch_Ground && Jump == false)
+        {
+            Jump = true;
+            Touch_Ground = false;
+            if (Run_Jump == 0)
+            {
+                Run_Jump = 4;
+            }
+            body.AddForce((Vector2.up) * Run_Jump * 100);
+        }
+        if (body.velocity.y == pre_y)
+        {
+            Touch_Ground = true;
+        }
+    }
+
 }
